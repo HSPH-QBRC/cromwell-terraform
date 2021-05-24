5b6781c3626b6a5791ec884822f9480f09fa587d
blawney <blawney@hsph.harvard.edu>
2021-05-24 16:22:18 -0400

    Initial commit. Needed to push changes to github to test build

---
+++ main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_version = "~> 0.14.8"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.60.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "random_id" "cromwell_random_suffix" {
+  byte_length = 4
+}
+
+resource "google_compute_network" "cromwell_network" {
+    name           = "cromwell-network-${random_id.cromwell_random_suffix.hex}"    
+}
+
+resource "google_compute_firewall" "cromwell_firewall" {
+  name    = "cromwell-${random_id.cromwell_random_suffix.hex}-ssh-firewall"
+  network = google_compute_network.cromwell_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  target_tags = ["cromwell-${random_id.cromwell_random_suffix.hex}-allow-ssh"]
+
+}
+
+# Allow http into that machine
+resource "google_compute_firewall" "cromwell_http" {
+  name    = "cromwell-${random_id.cromwell_random_suffix.hex}-http"
+  network = google_compute_network.cromwell_network.name
+
+  allow {
+    protocol                 = "tcp"
+    ports                    = ["8000"]
+  }
+ 
+  target_tags = ["cromwell-8000"]
+}
+
+resource "google_compute_instance" "cromwell" {
+  name                    = "cromwell-${random_id.cromwell_random_suffix.hex}"
+  machine_type            = var.cromwell_machine_config.machine_type
+  tags                    = ["cromwell-8000", "cromwell-${random_id.cromwell_random_suffix.hex}-allow-ssh"]
+
+  metadata_startup_script = templatefile("cromwell_provision.sh", 
+    { 
+      project_id = var.project_id,
+      cromwell_bucket = "gs://${var.cromwell_bucket}",
+      cromwell_db_name = var.cromwell_db_name,
+      cromwell_db_user = var.cromwell_db_user,
+      cromwell_db_password = var.cromwell_db_password
+    }
+  )
+
+  boot_disk {
+    initialize_params {
+      image = var.cromwell_os_image
+      size = var.cromwell_machine_config.disk_size_gb
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.cromwell_network.name
+
+    # This empty block allows us to get an IP
+    access_config {
+    }
+  }
+
+}
---
+++ vars.tf
@@ -0,0 +1,53 @@
+variable "project_id" {
+  description = "GCP project ID"
+}
+
+variable "region" {
+  default = "us-east4"
+}
+
+variable "zone" {
+  default = "us-east4-c"
+}
+
+variable "credentials_file" {
+  description = "Path to JSON file with GCP service account key"
+}
+
+variable "cromwell_machine_config" {
+  type    = object({
+                machine_type   = string
+                disk_size_gb   = number
+            })
+  default = {
+    machine_type = "e2-standard-2"
+    disk_size_gb = 20
+  }
+}
+
+variable "cromwell_os_image" {
+  description = "The operating system to use with the Cromwell VM"
+  default = "ubuntu-2004-focal-v20210325"
+}
+
+variable "cromwell_bucket" {
+  description = "Name of the bucket where Cromwell will place its files. Do NOT include the gs prefix."
+}
+
+variable "cromwell_db_name" {
+  description = "The name of the database."
+  type        = string
+  sensitive   = true
+}
+
+variable "cromwell_db_user" {
+  description = "The database user."
+  type        = string
+  sensitive   = true
+}
+
+variable "cromwell_db_password" {
+  description = "Password for the database"
+  type        = string
+  sensitive   = true
+}
